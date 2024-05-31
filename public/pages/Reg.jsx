c7002de73112e6d3dab062d298b229cafa71e73c
Alex Dechart <alex.arrow26@gmail.com>
2024-05-31 21:58:12 +0300

    исправленный регистрейшн

--- public/pages/Reg.jsx
+++ public/pages/Reg.jsx
@@ -1,75 +1,86 @@
 import "../CSS/normalize.css";
 import "../CSS/style_1.css"
+import "../CSS/style_lk.css"
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-function Reg() {
-  document.title = "Новости";
+const Reg = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Пароли не совпадают, попробуйте еще раз.");
+      return;
+    }
+
+    fetch("http://localhost:3000/Reg", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((json) => alert(json.success ? json.success : json.error))
+      .catch((err) => console.error("Error:", err));
+  };
+
   return (
-    <div className="Reg">
-      <header className="header">
-        <Link to="/home" className="header-logo">
-          <img src="img/LOGO.svg" className="logo" alt="Logo" /> //лого
-        </Link>
-        <ul className="header-menu"> //навигация и вход
-          <Link to="/home" className="button">Новости</Link>
-          <div className="button">База подкастов</div>
-          <Link to="/aboutus" className="button">О проекте</Link>
-          <Link to="/lk" className="button_lk">Вход</Link>
-        </ul>
-      </header>
-
-      <Route path="/home" component={Index.jsx} />
-      <Route path="/aboutus" component={AboutUs.jsx} />
-      <Route path="/lk" component={lk.jsx} />
-      
-      <body> 
-        <div className="container">
-            <h1>Регистрация</h1>
-            <form id="register-form">
-              <div className="input-group">
-                <label for="email" className="form-label">Электронная почта</label>
-                <input type="email" id="email" placeholder="Введите электронную почту" required />
-              </div>
-              <div className="input-group">
-                <label for="create-password" className="form-label">Создать пароль:</label>
-                <input type="password" id="create-password" placeholder="Введите пароль" required />
-              </div>
-              <div className="input-group">
-                <label for="confirm-password" className="form-label">Повторить пароль:</label>
-                <input type="password" id="confirm-password" placeholder="Повторите пароль" required />
-              </div>
-              <button type="submit" className="btn">Зарегистрироваться</button>
-            </form>
-            <a className="href" href = "/login">
-                <button type="submit" className="btn">Войти</button>
-            </a>
+    <div className="container">
+      <h1>Регистрация</h1>
+      <form id="Reg-form" onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label for="email" className="form-label">
+            Электронная почта
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Введите электронную почту"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </div>
-        <script>
-          document.getElementById('register-form').addEventListener('submit', function(e) {
-            e.preventDefault();
-            
-            const email = document.getElementById('email').value;
-            const password = document.getElementById('create-password').value;
-            const confirmPassword = document.getElementById('confirm-password').value;
-            
-            if(password !== confirmPassword) {
-              alert('Пароли не совпадают, попробуйте еще раз.');
-              return;
-            }
-            
-            fetch('http://localhost:3000/register', {
-              method: 'POST',
-              body: JSON.stringify({ email: email, password: password }),
-              headers: { 'Content-Type': 'application/json' }
-            })
-            .then(response => response.json())
-            .then(json => alert(json.success ? json.success : json.error))
-            .catch(err => console.error('Error:', err));
-          });
-        </script>
-      </body>
-
+        <div className="input-group">
+          <label for="create-password" className="form-label">
+            Создать пароль:
+          </label>
+          <input
+            type="password"
+            id="create-password"
+            placeholder="Введите пароль"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label for="confirm-password" className="form-label">
+            Повторить пароль:
+          </label>
+          <input
+            type="password"
+            id="confirm-password"
+            placeholder="Повторите пароль"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="btn">
+          Зарегистрироваться
+        </button>
+        <a className="href" href="/login">
+          <button type="submit" className="btn">
+            Войти
+          </button>
+        </a>
+      </form>
     </div>
   );
-}
+};
+
+export default Reg;
